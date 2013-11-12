83ac9c741904779c24b622ca566b72e790976378
gchan123 <gchan123@yahoo.com>
2013-11-11 22:57:48 -0500

    added in dummy job for Quartz scheduler

---
+++ src/main/java/com/ut/dph/jobs/JobA.java
@@ -0,0 +1,15 @@
+package com.ut.dph.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+ 
+public class JobA implements Job {
+ 
+	@Override
+	public void execute(JobExecutionContext context)
+		throws JobExecutionException {
+		System.out.println("Job A is runing");
+	}
+ 
+}
