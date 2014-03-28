1c9f30e3cb60630a9fd1544fa6442ab84f071980
gchan123 <gchan123@yahoo.com>
2014-03-28 03:46:03 -0400

    added js to send link to view detail audit report

--- src/main/webapp/dspResources/js/front-end/Health-e-Connect/upload.js
+++ src/main/webapp/dspResources/js/front-end/Health-e-Connect/upload.js
@@ -19,6 +19,15 @@ require(['./main'], function () {
            $('#page').val($(this).attr('rel'));
            $('#searchForm').submit();
         });
+        
+        //this function will submit the batchId for viewing detailed audit report
+        $(document).on('click', '.viewLink', function() {
+        	$('input[name="batchId"]').val($(this).attr('rel'));
+        	//somehow we need to submit form
+        	$('#searchForm').attr('action', 'auditReport');
+        	$('#searchForm').submit();
+        });
+        
 
         //This function will launch the new file upload overlay with a blank screen
         $(document).on('click', '.uploadFile', function() {
