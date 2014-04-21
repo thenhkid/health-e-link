23b5eedf4fc8c44fa081f286dd25a49509cb3ee3
Chad <chadmccue05@gmail.com>
2014-04-21 13:03:10 -0400

    - Added messageStatus to the transactionIn table

--- src/main/java/com/ut/dph/model/transactionIn.java
+++ src/main/java/com/ut/dph/model/transactionIn.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ut.dph.model;
 
 import java.util.Date;
@@ -22,28 +21,31 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 @Table(name = "TRANSACTIONIN")
 public class transactionIn {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID", nullable = false)
     private int id;
-    
+
     @Column(name = "BATCHID", nullable = false)
     private int batchId;
-    
+
     @Column(name = "CONFIGID", nullable = false)
     private Integer configId;
 
     @Column(name = "STATUSID", nullable = false)
     private int statusId;
-    
+
     @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
     @Column(name = "DATECREATED", nullable = false)
     private Date dateCreated = new Date();
-    
+
     @Column(name = "TRANSACTIONTARGETID", nullable = false)
     private int transactionTargetId = 0;
-    
+
+    @Column(name = "MESSAGESTATUS", nullable = false)
+    private int messageStatus = 1;
+
     public int getId() {
         return id;
     }
@@ -59,7 +61,7 @@ public class transactionIn {
     public void setbatchId(int batchId) {
         this.batchId = batchId;
     }
-    
+
     public int getconfigId() {
         return configId;
     }
@@ -67,7 +69,7 @@ public class transactionIn {
     public void setconfigId(int configId) {
         this.configId = configId;
     }
-    
+
     public int getstatusId() {
         return statusId;
     }
@@ -75,15 +77,15 @@ public class transactionIn {
     public void setstatusId(int statusId) {
         this.statusId = statusId;
     }
-    
+
     public Date getdateCreated() {
         return dateCreated;
     }
-    
+
     public void setdateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
     }
-    
+
     public int gettransactionTargetId() {
         return transactionTargetId;
     }
@@ -91,12 +93,20 @@ public class transactionIn {
     public void settransactionTargetId(int transactionTargetId) {
         this.transactionTargetId = transactionTargetId;
     }
-    
-    public Integer getConfigId() {
-		return configId;
-	}
 
-	public void setConfigId(Integer configId) {
-		this.configId = configId;
-	}
+    public Integer getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(Integer configId) {
+        this.configId = configId;
+    }
+    
+    public int getmessageStatus() {
+        return messageStatus;
+    }
+    
+    public void setmessageStatus(int messageStatus) {
+        this.messageStatus = messageStatus;
+    }
 }
