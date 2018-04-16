c7e7ed974d16dfcc03ff8480bb7b3a50709ef293
Chad M <chadmccue05@gmail.com>
2018-04-16 10:17:37 -0400

    -added system overview file

--- src/main/java/com/ut/healthelink/controller/mainController.java
+++ src/main/java/com/ut/healthelink/controller/mainController.java
@@ -9,6 +9,11 @@ import com.ut.healthelink.service.emailMessageManager;
 import com.ut.healthelink.service.newsArticleManager;
 import com.ut.healthelink.service.newsletterManager;
 import com.ut.healthelink.service.userManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +24,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -50,6 +57,24 @@ public class mainController {
     
     @Autowired
     private newsletterManager newslettermanager;
+    
+    
+    @RequestMapping(value = "/files/{file_name}", method = RequestMethod.GET)
+    public void getFile(@PathVariable("file_name") String fileName, HttpServletResponse response) throws FileNotFoundException {
+	try {
+	
+	    File file = ResourceUtils.getFile("classpath:files/"+fileName+".docx");
+	
+	    // get your file as InputStream
+	    InputStream is = new FileInputStream(file);
+	    // copy it to response's OutputStream
+	    org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+	    response.flushBuffer();
+	 } 
+	catch (IOException ex) {
+	   throw new RuntimeException("IOError writing file to output stream");
+	}
+    }
    
     
     /**
